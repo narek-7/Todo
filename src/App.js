0a42f9685104831d67677467f8d162d499367301
Narek <narek1985ch@gmail.com>
2022-03-10 00:45:35 +0100

    using useReduce hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import TodosList from "./components/todosList";
 import TodoHeader from "./components/todoHeader";
 import TodoFooter from "./components/todoFooter";
 import "./App.css";
 
+function reducer(state, action) {
+   if (action.type === "add") {
+      return [
+         ...state,
+         {
+            id: state[state.length - 1].id + 1,
+            title: action.payload.title,
+            completed: false,
+         },
+      ];
+   }
+   if (action.type === "delete") {
+      return state.filter((todo) => todo.id !== action.payload.id);
+   }
+   if (action.type === "toggle") {
+      return state.map((todo) => {
+         if (todo.id === action.payload.id) {
+            return { ...todo, completed: !todo.completed };
+         }
+         return todo;
+      });
+   }
+   if (action.type === "clear") {
+      return state.filter((todo) => !todo.completed);
+   }
+}
+
 function App() {
-   let [todosList, setTodos] = useState([
+   const [todosList, dispatch] = useReducer(reducer, [
       { id: 1, title: "Learn JS", completed: false },
       { id: 2, title: "Learn React", completed: false },
       { id: 3, title: "Learn Angular", completed: false },
@@ -14,46 +41,38 @@ function App() {
    ]);
 
    const handleToggle = (id) => {
-      setTodos(
-         (prevState) =>
-            (prevState = todosList.map((todo) => {
-               if (todo.id === id) {
-                  return { ...todo, completed: !todo.completed };
-               }
-               return todo;
-            }))
-      );
-   };
-
-   const generateId = () => {
-      return todosList[todosList.length - 1].id + 1;
+      dispatch({
+         type: "toggle",
+         payload: { id: id },
+      });
    };
 
    const addTodo = (title) => {
       if (title.trim()) {
-         setTodos([
-            ...todosList,
-            {
-               id: generateId(),
-               title: title,
-               completed: false,
-            },
-         ]);
+         dispatch({
+            type: "add",
+            payload: { title: title },
+         });
       }
-      console.log(todosList);
    };
 
    const clearCompleted = () => {
-      setTodos((prevState) => (prevState = todosList.filter((todo) => !todo.completed)));
+      dispatch({
+         type: "clear",
+      });
    };
 
    const deleteTodo = (id) => {
-      setTodos((prevState) => (prevState = todosList.filter((todo) => todo.id !== id)));
+      dispatch({
+         type: "delete",
+         payload: { id: id },
+      });
    };
    {
       return (
          <div className="container p-2">
             <div className="list shadow-lg p-3 mb-5 bg-body rounded">
+               <h1>Todos</h1>
                <TodoHeader addTodo={addTodo} />
                <TodosList
                   todosList={todosList}
